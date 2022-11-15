4acc8ede5f9b20b8b1ac9088d4964c0ed10567d8
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-15 17:36:47 +0900

    add delete function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import { collection, getDocs } from "firebase/firestore"
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore"
 import React, { useEffect, useState } from 'react'
-import { db } from "../firebase"
+import { auth, db } from "../firebase"
 import './Home.css'
 
 const Home = () => {
@@ -15,6 +15,11 @@ const Home = () => {
     getPosts();
   }, []);
 
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+    window.location.href = "/";
+  };
+
   return (
     <div className='homePage'>
       {postList.map((post) => {
@@ -28,7 +33,9 @@ const Home = () => {
             </div>
             <div className="nameAndDeleteButton">
               <h3>@{post.author.username}</h3>
-              <button>削除</button>
+              {post.author.id === auth.currentUser.uid && (
+                <button onClick={() => handleDelete(post.id)}>削除</button>
+              )}
             </div>
           </div>
         );
