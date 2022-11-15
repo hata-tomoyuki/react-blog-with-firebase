171d3e63e2dae3c6d69e19e88febd411676a5117
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-15 17:20:40 +0900

    get and show data in firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import { collection, getDocs } from "firebase/firestore"
+import React, { useEffect, useState } from 'react'
+import { db } from "../firebase"
 import './Home.css'
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    getPosts();
+  }, []);
+
   return (
     <div className='homePage'>
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>author</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">
+              {post.postText}
+            </div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   )
 }
